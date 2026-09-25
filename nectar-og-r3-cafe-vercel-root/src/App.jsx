c74cf6df0f034816c675dea6ba1d059169af8fd8
mahekshahadv-code <mahekshahadv@gmail.com>
2026-09-25 23:30:08 +0530

    Add useState import to App.jsx

--- nectar-og-r3-cafe-vercel-root/src/App.jsx
+++ nectar-og-r3-cafe-vercel-root/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import MenuPage from "./pages/MenuPage";
 import OwnerLogin from "./pages/OwnerLogin";
