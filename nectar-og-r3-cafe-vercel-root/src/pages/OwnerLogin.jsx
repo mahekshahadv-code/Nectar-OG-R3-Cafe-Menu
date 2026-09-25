cc09f239a0aeb346c50b8dae40ff97d63ddfd14c
mahekshahadv-code <mahekshahadv@gmail.com>
2026-09-25 22:07:19 +0530

    Add React import to OwnerLogin component

--- nectar-og-r3-cafe-vercel-root/src/pages/OwnerLogin.jsx
+++ nectar-og-r3-cafe-vercel-root/src/pages/OwnerLogin.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useEffect, useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 import { signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
